93a3775856043015f450bf473c046e5654c3fefa
kumarktest <92436442+kumarktest@users.noreply.github.com>
2021-10-13 21:14:28 +0100

    Removed the commented code

--- Automation Test/seyotestassignment/src/test/java/stepDefinitions/cleaningPatchesTest.java
+++ Automation Test/seyotestassignment/src/test/java/stepDefinitions/cleaningPatchesTest.java
@@ -28,9 +28,6 @@ public class cleaningPatchesTest {
     List<List<Integer>> listOfPatches = new ArrayList<>();
     String instructionsString;
 
-
-    // @Given("the roomsize {string}, coords {string},  patches {string}, {string} and {string} as instructions")
-   //@Given("the roomsize {string}, coords {string}, patches (.*) and {string} as instructions")
    @Given("the roomsize {string}, coords {string}, {string} as instructions")
     public void the_roomsize_coords_patches_and_as_instructions(String rsize, String coords, String instructions) {
        roomsize = transform(rsize);
@@ -38,35 +35,27 @@ public class cleaningPatchesTest {
        instructionsString = instructions;
     }
 
-    @And("the patches listofpatches")
+    @Given("the patches listofpatches")
     public void the_patches_listofpatches(DataTable dataTable) {
 
-       // System.out.println("DataTable got from scenario "+ dataTable);
         List<List<String>> data = dataTable.asLists();
-       // System.out.println("List of List of Strings after extraction from data table" + data);
-
+     
         int total = 0;
         int subListCount;
         for (List<String> sublist : data) {
             total += sublist.size();
             subListCount = sublist.size();
-           // System.out.println(subListCount);
-           // System.out.println("sublist"+ sublist);
+          
             for (String patch : sublist){
                 System.out.println(patch);
                 List<Integer> patchint = transform(patch);
                 listOfPatches.add(patchint);
             }
-          //  System.out.println(listOfPatches);
+        
         }
         csBody = new CleanSessionsRequest(roomsize,intialcoords,listOfPatches,instructionsString);
 
     }
-
-//    @Given("the roomsize [{Integer}], coords [{Integer}], patches [[{int}, {int}], [{int}, {int}], [{int}, {int}]] and {string} as instructions")
-//    public void the_roomsize_coords_patches_and_as_instructions(List<Integer> roomSize, List<Integer> coords,  List<List<Integer>> listOfPatches , String instructions) {
-//         csBody = new CleanSessionsRequest(roomSize,coords,listOfPatches,instructions);
-//    }
 
     @When("post request is made on {string} endpoint")
     public void post_request_is_made_on_endpoint(String url) {
@@ -86,13 +75,13 @@ public class cleaningPatchesTest {
         Assert.assertEquals(res.statusCode(),ExpectedstatusCode);
     }
 
-    @And("Final coords are {string} are returned")
+    @Then("Final coords are {string} are returned")
     public void final_coords_are_are_returned(String expectedcoords) {
         String actualCoords = res.getBody().path("coords").toString();
         Assert.assertEquals(actualCoords,expectedcoords);
     }
 
-    @And("Number of cleaned patches {int} is displayed")
+    @Then("Number of cleaned patches {int} is displayed")
     public void number_of_cleaned_patches_is_displayed(int expectedcleanPatches) {
         int actualPatches = res.getBody().path("patches");
         Assert.assertEquals(actualPatches,expectedcleanPatches);
